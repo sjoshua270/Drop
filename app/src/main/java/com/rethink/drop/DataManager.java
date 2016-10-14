f5d7efd08e2c5858b070c402f24f9948b0307426
Josh Sauder <joshua270@gmail.com>
2016-10-14 15:41:16 -0400

    Rearranges DataManager to allow detaching of listener

--- app/src/main/java/com/rethink/drop/DataManager.java
+++ app/src/main/java/com/rethink/drop/DataManager.java
@@ -7,6 +7,7 @@ import com.google.android.gms.tasks.OnSuccessListener;
 import com.google.firebase.database.ChildEventListener;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.storage.FirebaseStorage;
 import com.rethink.drop.models.Listing;
@@ -23,6 +24,8 @@ public class DataManager {
     public static HashMap<String, Listing> listings;
     public static HashMap<String, Boolean> loadedListings;
     private FirebaseStorage firebaseStorage;
+    private DataListener dataListener;
+    private DatabaseReference listingsRef;
     private float degreesPerMile = 0.01449275362f;
 
     DataManager() {
@@ -31,80 +34,88 @@ public class DataManager {
         listings = new HashMap<>();
         loadedListings = new HashMap<>();
         firebaseStorage = FirebaseStorage.getInstance();
-        FirebaseDatabase.getInstance()
-                        .getReference()
-                        .child("listings")
-                        .orderByChild("timestamp")
-                        .limitToFirst(100)
-                        .addChildEventListener(new ChildEventListener() {
-                            @Override
-                            public void onChildAdded(DataSnapshot dataSnapshot, String s) {
-                                final String key = dataSnapshot.getKey();
-                                Listing listing = dataSnapshot.getValue(Listing.class);
-                                keys.add(key);
-                                listings.put(key, listing);
-                                if (listing.getImageURL() != null) {
-                                    loadedListings.put(key, false);
-                                    if (!listing.getImageURL().equals("")) {
-                                        firebaseStorage.getReferenceFromUrl(listing.getImageURL())
-                                                       .getBytes(4 * (1024 * 1024))
-                                                       .addOnSuccessListener(new OnSuccessListener<byte[]>() {
-                                                           @Override
-                                                           public void onSuccess(final byte[] bytes) {
-                                                               Bitmap bmp = BitmapFactory.decodeByteArray(bytes, 0, bytes.length);
-                                                               int dimens = Math.min(bmp.getWidth(), bmp.getHeight());
-                                                               imageBitmaps.put(key, Bitmap.createBitmap(bmp, 0, 0, dimens, dimens));
-                                                               loadedListings.put(key, true);
-                                                               listingsAdapter.notifyDataSetChanged();
-                                                           }
-                                                       });
-                                    }
-                                }
-                                listingsAdapter.notifyDataSetChanged();
-                            }
+        listingsRef = FirebaseDatabase.getInstance()
+                                      .getReference()
+                                      .child("listings");
+        listingsRef.orderByChild("timestamp")
+                   .limitToFirst(100);
+        dataListener = new DataListener();
+        listingsRef.addChildEventListener(dataListener);
+    }
 
-                            @Override
-                            public void onChildChanged(DataSnapshot dataSnapshot, String s) {
-                                final String key = dataSnapshot.getKey();
-                                Listing listing = dataSnapshot.getValue(Listing.class);
-                                listings.put(key, listing);
-                                if (!listing.getImageURL().equals("")) {
-                                    final long ONE_KILOBYTE = 1024;
-                                    firebaseStorage.getReferenceFromUrl(listing.getImageURL())
-                                                   .getBytes(512 * ONE_KILOBYTE)
-                                                   .addOnSuccessListener(new OnSuccessListener<byte[]>() {
-                                                       @Override
-                                                       public void onSuccess(final byte[] bytes) {
-                                                           Bitmap bmp = BitmapFactory.decodeByteArray(bytes, 0, bytes.length);
-                                                           int dimens = Math.min(bmp.getWidth(), bmp.getHeight());
-                                                           imageBitmaps.put(key, Bitmap.createBitmap(bmp, 0, 0, dimens, dimens));
-                                                           listingsAdapter.notifyDataSetChanged();
-                                                       }
-                                                   });
-                                }
-                                listingsAdapter.notifyDataSetChanged();
-                            }
+    public void detachListeners() {
+        listingsRef.removeEventListener(dataListener);
+    }
 
-                            @Override
-                            public void onChildRemoved(DataSnapshot dataSnapshot) {
-                                final String key = dataSnapshot.getKey();
-                                String imageURL = listings.get(key).getImageURL();
-                                if (imageURL != null && !imageURL.equals("")) {
-                                    firebaseStorage.getReferenceFromUrl(listings.get(key).getImageURL()).delete();
-                                }
-                                keys.remove(key);
-                                listings.remove(key);
-                                imageBitmaps.remove(key);
-                                listingsAdapter.notifyDataSetChanged();
-                            }
+    private class DataListener
+            implements ChildEventListener {
+        @Override
+        public void onChildAdded(DataSnapshot dataSnapshot, String s) {
+            final String key = dataSnapshot.getKey();
+            Listing listing = dataSnapshot.getValue(Listing.class);
+            keys.add(key);
+            listings.put(key, listing);
+            if (listing.getImageURL() != null) {
+                loadedListings.put(key, false);
+                if (!listing.getImageURL().equals("")) {
+                    firebaseStorage.getReferenceFromUrl(listing.getImageURL())
+                                   .getBytes(4 * (1024 * 1024))
+                                   .addOnSuccessListener(new OnSuccessListener<byte[]>() {
+                                       @Override
+                                       public void onSuccess(final byte[] bytes) {
+                                           Bitmap bmp = BitmapFactory.decodeByteArray(bytes, 0, bytes.length);
+                                           int dimens = Math.min(bmp.getWidth(), bmp.getHeight());
+                                           imageBitmaps.put(key, Bitmap.createBitmap(bmp, 0, 0, dimens, dimens));
+                                           loadedListings.put(key, true);
+                                           listingsAdapter.notifyDataSetChanged();
+                                       }
+                                   });
+                }
+            }
+            listingsAdapter.notifyDataSetChanged();
+        }
 
-                            @Override
-                            public void onChildMoved(DataSnapshot dataSnapshot, String s) {
-                            }
+        @Override
+        public void onChildChanged(DataSnapshot dataSnapshot, String s) {
+            final String key = dataSnapshot.getKey();
+            Listing listing = dataSnapshot.getValue(Listing.class);
+            listings.put(key, listing);
+            if (!listing.getImageURL().equals("")) {
+                final long ONE_KILOBYTE = 1024;
+                firebaseStorage.getReferenceFromUrl(listing.getImageURL())
+                               .getBytes(512 * ONE_KILOBYTE)
+                               .addOnSuccessListener(new OnSuccessListener<byte[]>() {
+                                   @Override
+                                   public void onSuccess(final byte[] bytes) {
+                                       Bitmap bmp = BitmapFactory.decodeByteArray(bytes, 0, bytes.length);
+                                       int dimens = Math.min(bmp.getWidth(), bmp.getHeight());
+                                       imageBitmaps.put(key, Bitmap.createBitmap(bmp, 0, 0, dimens, dimens));
+                                       listingsAdapter.notifyDataSetChanged();
+                                   }
+                               });
+            }
+            listingsAdapter.notifyDataSetChanged();
+        }
 
-                            @Override
-                            public void onCancelled(DatabaseError databaseError) {
-                            }
-                        });
+        @Override
+        public void onChildRemoved(DataSnapshot dataSnapshot) {
+            final String key = dataSnapshot.getKey();
+            String imageURL = listings.get(key).getImageURL();
+            if (imageURL != null && !imageURL.equals("")) {
+                firebaseStorage.getReferenceFromUrl(listings.get(key).getImageURL()).delete();
+            }
+            keys.remove(key);
+            listings.remove(key);
+            imageBitmaps.remove(key);
+            listingsAdapter.notifyDataSetChanged();
+        }
+
+        @Override
+        public void onChildMoved(DataSnapshot dataSnapshot, String s) {
+        }
+
+        @Override
+        public void onCancelled(DatabaseError databaseError) {
+        }
     }
 }
