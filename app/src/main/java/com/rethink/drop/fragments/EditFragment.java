7ba61703401624538b3519f29bd3bddeceaa5eb2
Josh Sauder <joshua270@gmail.com>
2016-10-18 16:08:02 -0400

    Has EditFragment ask for permission to read files

--- app/src/main/java/com/rethink/drop/fragments/EditFragment.java
+++ app/src/main/java/com/rethink/drop/fragments/EditFragment.java
@@ -1,7 +1,9 @@
 package com.rethink.drop.fragments;
 
+import android.Manifest;
 import android.app.ProgressDialog;
 import android.content.Intent;
+import android.content.pm.PackageManager;
 import android.graphics.Bitmap;
 import android.net.Uri;
 import android.os.Bundle;
@@ -10,6 +12,7 @@ import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
 import android.support.design.widget.CoordinatorLayout;
 import android.support.design.widget.Snackbar;
+import android.support.v4.app.ActivityCompat;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuInflater;
@@ -85,16 +88,30 @@ public class EditFragment
         imageView.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent();
-                intent.setType("image/*");
-                intent.setAction(Intent.ACTION_GET_CONTENT);
-                startActivityForResult(Intent.createChooser(intent, "Select Picture"), GALLERY_REQUEST);
+                if (ActivityCompat.checkSelfPermission(
+                        getContext(),
+                        Manifest.permission.READ_EXTERNAL_STORAGE)
+                        == PackageManager.PERMISSION_GRANTED) {
+                    getImage();
+                } else {
+                    ActivityCompat.requestPermissions(
+                            getActivity(),
+                            new String[]{Manifest.permission.READ_EXTERNAL_STORAGE},
+                            0);
+                }
             }
         });
 
         cLayout = (CoordinatorLayout) getActivity().findViewById(R.id.coordinator);
 
         return fragmentView;
+    }
+
+    public void getImage() {
+        Intent intent = new Intent();
+        intent.setType("image/*");
+        intent.setAction(Intent.ACTION_GET_CONTENT);
+        startActivityForResult(Intent.createChooser(intent, "Select Picture"), GALLERY_REQUEST);
     }
 
     /**
