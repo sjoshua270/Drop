593f5e378038eead50038bdccd5a139b101e24f6
Josh Sauder <joshua270@gmail.com>
2016-10-25 10:49:59 -0400

    Removes Globals and closes down access for distance() in Utilities

--- app/src/main/java/com/rethink/drop/Globals.java
+++
@@ -1,4 +0,0 @@
-package com.rethink.drop;
-
-class Globals {
-}
--- app/src/main/java/com/rethink/drop/Utilities.java
+++ app/src/main/java/com/rethink/drop/Utilities.java
@@ -37,8 +37,8 @@ public class Utilities {
      *
      * @return Distance in Meters
      **/
-    public static double distance(double lat1, double lat2, double lon1,
-                                  double lon2, double el1, double el2) {
+    private static double distance(double lat1, double lat2, double lon1,
+                                   double lon2, double el1, double el2) {
 
         final int R = 6371; // Radius of the earth
 
