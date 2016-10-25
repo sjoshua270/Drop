77e4105f4423dc49f08264816757e71d10562833
Josh Sauder <joshua270@gmail.com>
2016-10-25 11:11:25 -0400

    Adds first test class

---
+++ app/src/test/java/com/rethink/AppTest.java
@@ -0,0 +1,39 @@
+package com.rethink;
+
+import com.rethink.drop.Utilities;
+
+import org.junit.Test;
+
+import java.util.Locale;
+
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertTrue;
+
+public class AppTest {
+    @Test
+    public void checkDistanceCalcCorrect() {
+        double KM = Utilities.distanceInKilometers(
+                39.952584,
+                38.449569,
+                -75.165222,
+                -78.868916,
+                0.0, 0.0);
+        double MI = Utilities.distanceInMiles(
+                39.952584,
+                38.449569,
+                -75.165222,
+                -78.868916,
+                0.0, 0.0);
+        assertTrue(360.1 < KM && KM < 360.4);
+        assertTrue(223.8 < MI && MI < 224.0);
+    }
+
+    @Test
+    public void checkUseMetric() {
+        Locale US = Locale.US;
+        Locale CHINA = Locale.CHINA;
+
+        assertFalse(Utilities.useMetric(US));
+        assertTrue(Utilities.useMetric(CHINA));
+    }
+}
