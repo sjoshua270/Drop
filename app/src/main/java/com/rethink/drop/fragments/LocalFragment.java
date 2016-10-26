c76a0c7d1dfcfba2bcbdbaf0ffa4a7e31f5da05a
Josh Sauder <joshua270@gmail.com>
2016-10-26 16:53:29 -0400

    Rename menu_main to menu_local

--- app/src/main/java/com/rethink/drop/fragments/LocalFragment.java
+++ app/src/main/java/com/rethink/drop/fragments/LocalFragment.java
@@ -55,7 +55,7 @@ public class LocalFragment
 
     @Override
     public void onCreateOptionsMenu(Menu menu, MenuInflater inflater) {
-        inflater.inflate(R.menu.menu_main, menu);
+        inflater.inflate(R.menu.menu_local, menu);
         super.onCreateOptionsMenu(menu, inflater);
     }
 }
