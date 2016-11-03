d5f03229dea70802a229cfdbf2623d62d736dccd
Josh Sauder <joshua270@gmail.com>
2016-11-02 21:02:31 -0400

    14: Switches to new loader to fill SubsamplingScaleImageView

--- app/src/main/java/com/rethink/drop/fragments/ImageFragment.java
+++ app/src/main/java/com/rethink/drop/fragments/ImageFragment.java
@@ -8,11 +8,14 @@ import android.support.v4.view.ViewCompat;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.ImageView;
 
+import com.davemorrissey.labs.subscaleview.ImageSource;
+import com.davemorrissey.labs.subscaleview.SubsamplingScaleImageView;
+import com.nostra13.universalimageloader.core.ImageLoader;
+import com.nostra13.universalimageloader.core.ImageLoaderConfiguration;
+import com.nostra13.universalimageloader.core.listener.SimpleImageLoadingListener;
 import com.rethink.drop.R;
 import com.rethink.drop.models.Listing;
-import com.squareup.picasso.Picasso;
 
 import static com.rethink.drop.DataManager.imageBitmaps;
 import static com.rethink.drop.DataManager.listings;
@@ -21,7 +24,7 @@ import static com.rethink.drop.models.Listing.KEY;
 public class ImageFragment
         extends Fragment {
     Bitmap imageBitmap;
-    ImageView imageView;
+    SubsamplingScaleImageView imageView;
 
     public static ImageFragment newInstance(String key) {
         Bundle args = new Bundle();
@@ -36,15 +39,18 @@ public class ImageFragment
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
         super.onCreateView(inflater, container, savedInstanceState);
         View v = inflater.inflate(R.layout.fragment_image, container, false);
-        imageView = (ImageView) v.findViewById(R.id.full_image);
-        imageView.setImageBitmap(imageBitmap);
+        imageView = (SubsamplingScaleImageView) v.findViewById(R.id.full_image);
         Listing listing = listings.get(getArguments().getString(KEY));
         String imageUrl = listing.getImageURL() == null ? "" : listing.getImageURL();
         if (!imageUrl.equals("") && container != null) {
-            Picasso.with(container.getContext())
-                   .load(imageUrl)
-                   .placeholder(R.drawable.ic_photo_camera_white_24px)
-                   .into(imageView);
+            ImageLoader imageLoader = ImageLoader.getInstance();
+            imageLoader.init(ImageLoaderConfiguration.createDefault(getContext()));
+            imageLoader.loadImage(imageUrl, new SimpleImageLoadingListener() {
+                @Override
+                public void onLoadingComplete(String imageUri, View view, Bitmap loadedImage) {
+                    imageView.setImage(ImageSource.bitmap(loadedImage));
+                }
+            });
         }
 
         ViewCompat.setTransitionName(imageView, "image");
