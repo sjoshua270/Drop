a3acd8dc9bb89a4f74dba3c6b44eec5653560b5f
Josh Sauder <joshua270@gmail.com>
2016-11-16 20:46:53 -0500

    23: Adds catch for null Drop data
    This catch deletes the GeoFire location

--- app/src/main/java/com/rethink/drop/adapters/DropAdapter.java
+++ app/src/main/java/com/rethink/drop/adapters/DropAdapter.java
@@ -14,6 +14,7 @@ import android.view.WindowManager;
 import android.widget.ImageView;
 import android.widget.TextView;
 
+import com.firebase.geofire.GeoFire;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.FirebaseDatabase;
@@ -81,6 +82,12 @@ public class DropAdapter
                                     holder.imageView.setPadding(0, 0, 0, 0);
                                     holder.title.setText(drop.getTitle());
                                     holder.desc.setText(drop.getDescription());
+                                } else {
+                                    GeoFire geoFire = new GeoFire(
+                                            FirebaseDatabase.getInstance()
+                                                            .getReference()
+                                                            .child("geoFire"));
+                                    geoFire.removeLocation(key);
                                 }
                             }
 
